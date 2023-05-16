112d989f2e5528ba42eea7ef84e83e2d0c1360c0
lhwdev <lhwdev6@outlook.com>
2023-05-16 23:14:36 +0900

    blog

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,9 @@
+import { NextComponentType } from "next";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 
 interface CustomAppProps extends AppProps {
-  name?: string;
+  Component: NextComponentType & { route?: { name: string } };
 }
 
 export default function MyApp({ Component, pageProps }: CustomAppProps) {
@@ -10,6 +11,7 @@ export default function MyApp({ Component, pageProps }: CustomAppProps) {
     <>
       <Head>
         {/* 이건 _app.js에 넣어도 되지만 이런게 가능하다는 걸 보여주기 위해서 */}
+        <title>{Component.route?.name}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="theme-color" content="#3147F5" />
       </Head>
